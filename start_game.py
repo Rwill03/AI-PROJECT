d8ffa37b90014d010d64032c123adb0780f9db36
William Rogov <flexerwithepether@gmail.com>
2023-11-23 14:10:58 +0100

    minor adjustments

--- start_game.py
+++ start_game.py
@@ -19,7 +19,7 @@ game = Game(window, width, height)
 run = True
 clock= pygame.time.Clock()
 while run: 
-    clock.tick(60)
+    clock.tick(80)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
